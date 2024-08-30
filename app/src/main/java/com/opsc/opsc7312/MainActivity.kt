c83db103f254dedc06ea66b84eb608cfe6b39809
ST10215069-Nkosinathi-Ngozo <ST10215069@vcconnect.edu.za>
2024-08-30 19:45:46 +0200

    settings screen

--- app/src/main/java/com/opsc/opsc7312/MainActivity.kt
+++ app/src/main/java/com/opsc/opsc7312/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.opsc.opsc7312
 
+import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.example.app.LanguageFragment
+import com.google.android.material.navigation.NavigationView
 import com.opsc.opsc7312.databinding.ActivityMainBinding
 import com.opsc.opsc7312.model.data.Category
+import com.opsc.opsc7312.view.fragment.AnalyticsFragment
 import com.opsc.opsc7312.view.fragment.CategoriesFragment
 import com.opsc.opsc7312.view.fragment.CreateGoalFragment
 import com.opsc.opsc7312.view.fragment.GoalsFragment
@@ -18,45 +26,107 @@ import com.opsc.opsc7312.view.fragment.HomeFragment
 import com.opsc.opsc7312.view.fragment.SettingsFragment
 import com.opsc.opsc7312.view.fragment.TransactionsFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var drawerLayout: DrawerLayout
 
-    private lateinit var toolbar: Toolbar
+    //private lateinit var toolbar: Toolbar
+
+    private lateinit var  navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+        val isDarkMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+
+        tintIconForDarkMode(findViewById(R.id.back_button), isDarkMode)
+        tintIconForDarkMode(findViewById(R.id.nav_drawer_opener), isDarkMode)
+
+
         drawerLayout = findViewById<DrawerLayout>(R.id.main)
 
-        toolbar = findViewById<Toolbar>(R.id.nav_toolbar)
-        setSupportActionBar(toolbar)
+        navigationView = findViewById<NavigationView>(R.id.nav_view)
+
+        //toolbar = findViewById<Toolbar>(R.id.nav_toolbar)
+        //setSupportActionBar(toolbar)
         setupBottomNavigation()
+
+        setupNavigationView()
+
+        findViewById<ImageButton>(R.id.back_button).setOnClickListener {
+            onBackPressed()
+        }
+
+        // Handle the navigation drawer opener click
+        findViewById<ImageButton>(R.id.nav_drawer_opener).setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.END)
+        }
+
+        binding.fab.setOnClickListener {
+            //changeCurrentFragment(GoalsFragment(), "Add Transaction")
+        }
     }
 
     private fun setupBottomNavigation() {
 
-        changeCurrentFragment(HomeFragment())
+        changeCurrentFragment(HomeFragment(), "Home")
 
         // Code for when a different button is pressed on the navigation menu
         binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.home -> changeCurrentFragment(HomeFragment())
-                R.id.transactions -> changeCurrentFragment(TransactionsFragment())
-                R.id.add -> changeCurrentFragment(GoalsFragment())
-                R.id.analytics -> changeCurrentFragment(CreateGoalFragment())
-                R.id.settings -> changeCurrentFragment(SettingsFragment())
+                R.id.home -> changeCurrentFragment(HomeFragment(), "Home")
+                R.id.transactions -> changeCurrentFragment(TransactionsFragment(), "Transactions")
+                R.id.add -> changeCurrentFragment(GoalsFragment(), "Add Transaction")
+                R.id.analytics -> changeCurrentFragment(AnalyticsFragment(), "Analytics")
+                R.id.settings -> changeCurrentFragment(SettingsFragment(), "Settings")
             }
             true
         }
     }
-    private fun changeCurrentFragment(fragment: Fragment) {
+    private fun changeCurrentFragment(fragment: Fragment, title: String) {
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.frame_layout, fragment)
             commit()
         }
+        // Set the title of the toolbar
+        binding.toolbarTitle.text = title
     }
+
+
+    private fun setupNavigationView() {
+
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+
+            R.id.nav_investment -> changeCurrentFragment(CategoriesFragment(), "Investments")
+            R.id.nav_categories -> changeCurrentFragment(CategoriesFragment(), "Categories")
+            R.id.nav_goal -> changeCurrentFragment(GoalsFragment(), "Goals")
+            R.id.nav_logout -> {
+                Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, MainActivity::class.java))
+
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.END)
+        return true
+    }
+
+    private fun tintIconForDarkMode(imageButton: ImageButton, isDarkMode: Boolean) {
+        val color = if (isDarkMode) {
+            getColor(R.color.white)  // Use white for dark mode
+        } else {
+            getColor(R.color.dark_grey)  // Use black for light mode
+        }
+        imageButton.setColorFilter(color)
+    }
+
 }
--- app/src/main/java/com/opsc/opsc7312/view/fragment/SettingsFragment.kt
+++ app/src/main/java/com/opsc/opsc7312/view/fragment/SettingsFragment.kt
@@ -12,62 +12,50 @@ import com.example.app.LanguageFragment
 import com.example.app.ui.security.SecurityFragment
 import com.example.yourapp.ThemeFragment
 import com.opsc.opsc7312.R
+import com.opsc.opsc7312.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
+
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    ): View {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+        buttonEvents()
+
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        // Navigate to LanguageFragment when the language section is clicked
-        val languageSection = view.findViewById<LinearLayout>(R.id.language_section)
-        languageSection.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.frame_layout, LanguageFragment())
-                .addToBackStack(null)
-                .commit()
+    private fun buttonEvents(){
+        binding.accountSection.setOnClickListener {
+            changeCurrentFragment(ProfileFragment())
         }
 
-        // Navigate to SecurityFragment when the security section is clicked
-        val securitySection = view.findViewById<LinearLayout>(R.id.security_section)
-        securitySection.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.frame_layout, SecurityFragment())
-                .addToBackStack(null)
-                .commit()
+        binding.languageSection.setOnClickListener {
+            changeCurrentFragment(LanguageFragment())
         }
 
-        // Navigate to NotificationsFragment when the notifications section is clicked
-        val notificationsSection = view.findViewById<LinearLayout>(R.id.notifications_section)
-        notificationsSection.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.frame_layout, NotificationsFragment())
-                .addToBackStack(null)
-                .commit()
+        binding.securitySection.setOnClickListener {
+            changeCurrentFragment(SecurityFragment())
         }
 
-        // Navigate to ProfileFragment when the Username section is clicked
-        val usernameSection = view.findViewById<LinearLayout>(R.id.account_section)
-        usernameSection.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.frame_layout, ProfileFragment())
-                .addToBackStack(null)
-                .commit()
+        binding.appearanceSection.setOnClickListener {
+            changeCurrentFragment(ThemeFragment())
         }
 
-        // Navigate to ThemeFragment when the appearance/theme section is clicked
-        val appearanceSection = view.findViewById<LinearLayout>(R.id.appearance_section)
-        appearanceSection.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.frame_layout, ThemeFragment())
-                .addToBackStack(null)
-                .commit()
+        binding.notificationsSection.setOnClickListener {
+            changeCurrentFragment(NotificationsFragment())
         }
+    }
+
+    private fun changeCurrentFragment(fragment: Fragment) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
